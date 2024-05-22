0f5107ebdffd20362b12c3c5488509d06a8675ae
humandad <55447225+humandad@users.noreply.github.com>
2024-05-22 19:10:00 -0400

    studio bot

---
+++ bundle/bots/runaction/studio/bot.ts
@@ -0,0 +1,34 @@
+import { RunActionBotApi } from "@uesio/bots"
+
+export default function studio(bot: RunActionBotApi) {
+	const params = bot.params.getAll()
+	const actionName = bot.getActionName()
+
+	bot.log.info("Name", actionName)
+	bot.log.info("PARAMS", params)
+
+	if (actionName !== "createOrder") {
+		bot.addError("unsupported action name: " + actionName)
+		return
+	}
+
+	/*
+	// Call API to create order
+	const result = bot.http.request({
+		method: "POST",
+		url: bot.getIntegration().getBaseURL() + "/api/v1/orders",
+		body: {
+			lineItems: itemNumbers,
+			amount: amount,
+		},
+	})
+	if (result.code !== 200) {
+		bot.addError("could not place order: " + result.status)
+		return
+	}
+	const orderDetails = result.body as OrderDetails
+	const { orderNumber } = orderDetails
+
+    bot.addResult("orderNumber", orderNumber)
+	*/
+}
